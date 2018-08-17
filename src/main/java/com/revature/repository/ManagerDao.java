4554edb0753e24b3706a97c453622974ee52c1b9
Andrew <jubydoo@gmail.com>
2018-08-16 21:29:55 -0500

    Untested ManagerDao
    Wrote up ManagerDao. This is untested, and since I'm just now learning
    Hibernate I'm sure it'll break somehow. But I wanted to get this up on
    GitHub so the rest of the team can take a look.

---
+++ src/main/java/com/revature/repository/ManagerDao.java
@@ -0,0 +1,80 @@
+package com.revature.repository;
+
+import java.util.List;
+
+import org.hibernate.Query;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.springframework.stereotype.Repository;
+
+import com.revature.beans.Manager;;
+
+@Repository
+public class ManagerDao {
+	private SessionFactory sessionFactory;
+
+	public void setSessionFactory(SessionFactory sessionFactory) {
+		this.sessionFactory = sessionFactory;
+	}
+
+	@SuppressWarnings("unchecked")
+	public List<Manager> getManagers() {
+		Session s = sessionFactory.getCurrentSession();
+		return s.createQuery("from Mananger").list();
+	}
+
+	public Manager getManagerById(int id) {
+		Session s = sessionFactory.getCurrentSession();
+		Query q = s.createQuery("from Manager where manager_id = :id");
+		q.setParameter("id", id);
+		return (Manager) q.uniqueResult();
+	}
+
+	public Manager getManagerByName(String firstname, String lastname) {
+		Session s = sessionFactory.getCurrentSession();
+		Query q = s.createQuery("from Manager where fname = :fname and lname = :lname");
+		q.setParameter("fname", firstname);
+		q.setParameter("lname", lastname);
+		return (Manager) q.uniqueResult();
+	}
+
+	public void addManager(Manager manager) {
+		Session s = sessionFactory.getCurrentSession();
+		Transaction tx = s.beginTransaction();
+
+		s.persist(manager);
+
+		tx.commit();
+	}
+
+	public void deleteManager(Manager manager) {
+		Session s = sessionFactory.getCurrentSession();
+		Transaction tx = s.beginTransaction();
+
+		s.delete(manager);
+
+		tx.commit();
+	}
+
+	/*
+	 * * if manager is detached, it is copied upon an existing persistent entity;
+	 * * if manager is transient, it is copied upon a newly created persistent
+	 * 		entity; this operation cascades for all relations with cascade=MERGE or
+	 * 		cascade=ALL mapping; 
+	 * * if the entity is persistent, then this method call
+	 * 		does not have effect on it (but the cascading still takes place).
+	 * (explanation of merge() from
+	 * https://www.baeldung.com/hibernate-save-persist-update-merge-saveorupdate)
+	 */
+	public Manager updateManager(Manager manager) {
+		Session s = sessionFactory.getCurrentSession();
+		Transaction tx = s.beginTransaction();
+
+		Manager m = (Manager) s.merge(manager);
+
+		tx.commit();
+
+		return m;
+	}
+}
